afc141290953229e71d62fa322ebdb8fa1ffb28a
Fearless <489368600@qq.com>
2021-08-02 15:02:58 +0800

    登录页面简单搭建

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,22 @@
 // pages/login/login.js
 Page({
+    
+    // 微信登录
+    wechatLogin: function() {
+        console.log("微信登录");
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    // 豆瓣已有帐号登录
+    doubanLogin: function() {
+        console.log("豆瓣已有帐号登录");
+    },
 
-  },
+    // 豆瓣使用协议/隐私协议
+    openProtocol: function() {
+        console.log("豆瓣使用协议/隐私协议");
+        wx.navigateTo({
+          url: '/pages/agreement/agreement',
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
