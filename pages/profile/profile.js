c1e21f264d2db54126f90c8a151d2c082f75dce4
Fearless <489368600@qq.com>
2021-08-03 15:20:13 +0800

    抽取优化

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,4 +1,50 @@
 // pages/profile/profile.js
 Page({
-  
+  data: {
+    items: [
+      {
+        icon: 'ic_cat_movie.png',
+        title: '观影分析',
+        count: 0,
+        has: '看过',
+        mark: '标记10部影片\n开启观影分析'
+      },
+      {
+        icon: 'ic_cat_book.png',
+        title: '读书分析',
+        count: 0,
+        has: '读过',
+        mark: '标记10本书\n开启读书分析'
+      },
+      {
+        icon: 'ic_cat_music.png',
+        title: '音乐分析',
+        count: 0,
+        has: '听过',
+        mark: '标记10张唱片\n开启音乐分析'
+      },     
+    ]
+  },
+
+  // 立即体验
+  immediatelyBegin: function(event) {
+    // event.currentTarget.id == 'item-0'
+    console.log(event);
+    const idx = event.currentTarget.dataset.index;
+    if (idx == 0) {
+      console.log('观影分析');
+    } else if (idx == 1) {
+      console.log('读书分析');
+    } else if (idx == 2) {
+      console.log('音乐分析');
+    }
+  },
+
+  login: function() {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
+  }
+
+
 })
